92f80a40d09f7b6feedb6a6392876f84573b8043
Pranjal <pgupta3_be19@thapar.edu>
2021-03-21 05:10:02 +0530

    configparser

--- libpygit.py
+++ libpygit.py
@@ -137,3 +137,14 @@ class GitRepository(object):
             config.write(f)
 
         return repo
+
+    
+    def repo_default_config():
+        ret = configparser.ConfigParser()
+
+        ret.add_section("core")
+        ret.set("core", "repositoryformatversion", "0")
+        ret.set("core", "filemode", "false")
+        ret.set("core", "bare", "false")
+
+        return ret
